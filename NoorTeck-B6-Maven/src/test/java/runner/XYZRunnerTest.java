4812e190f5f179a75be11f71fe1981ccf35f470d
buck <buck@andrews-mbp.lan>
2022-05-19 22:07:18 -0400

    5-19 committ

--- NoorTeck-B6-Maven/src/test/java/runner/XYZRunnerTest.java
+++ NoorTeck-B6-Maven/src/test/java/runner/XYZRunnerTest.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 
 @CucumberOptions (
-
+//add nane 2
 //add name
 				features = "src/test/resources/OrangeHRM/",
 				glue = {"steps"},
